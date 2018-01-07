93c9901f1e302a76cd8e4bdcdbf2e0b65724fd7a
mr-joo <skyish912@gmail.com>
2018-01-07 19:01:56 +0900

    calculator

--- src/Calculator/calc_action.js
+++ src/Calculator/calc_action.js
@@ -1,6 +1,7 @@
 let prevNum = 0;
 let currentNum = 0;
 let operator = undefined;
+let digit = 1;
 
 $(document).ready(function () {
 
@@ -10,6 +11,7 @@ $(document).ready(function () {
         prevNum = 0;
         currentNum = 0;
         operator = undefined;
+        digit = 1;
     });
 
     $("#alert").click(function () {
@@ -17,36 +19,56 @@ $(document).ready(function () {
     });
 
     $("#confirm").click(function () {
-        let confirm = prompt("수식을 입력해주세요.", "");
-        let input = confirm;
+        let input = prompt("수식을 입력해주세요.", "");
+        for (let i = 0; i < input.length; i++) {
+            let findVal = input.charAt(i);
+            console.log($("input[value='" + findVal + "']"));
+            $("input[value='" + findVal + "']").click();
+        }
+        $("#equal").click();
     });
 
+    $("#dot").click(function () {
+        if (digit == 1) {
+            digit = 0.1;
+        } else {
+            //do nothing
+        }
+    })
+
     $(".num > input").click(function () {
-        let new_num = parseInt($(this).val());
-        currentNum = currentNum * 10 + new_num;
-        $("#display").val(currentNum);
+        if (digit == 1) {
+            let new_num = parseInt($(this).val());
+            currentNum = currentNum * 10 + new_num;
+            $("#display").val(currentNum);
+        } else {
+            let new_num = parseInt($(this).val());
+            currentNum = currentNum + new_num * digit;
+            $("#display").val(parseFloat(currentNum));
+            digit = 1;
+        }
+
     });
 
     $(".calculation > input").click(function () {
-    console.log(operator);
         if (operator != undefined) {
-             switch (operator) {
-             case "+" :
-                      prevNum += currentNum;
-                      break;
+            switch (operator) {
+                case "+" :
+                    prevNum += currentNum;
+                    break;
 
-             case "-" :
-                      prevNum -= currentNum;
-                      break;
+                case "-" :
+                    prevNum -= currentNum;
+                    break;
 
-             case "*" :
-                       prevNum *= currentNum;
-                       break;
+                case "*" :
+                    prevNum *= currentNum;
+                    break;
 
-             case "/" :
-             prevNum /= currentNum;
-             break;
-             }
+                case "/" :
+                    prevNum /= currentNum;
+                    break;
+            }
             $("#display").val(prevNum);
         } else {
             prevNum = currentNum;
@@ -54,9 +76,10 @@ $(document).ready(function () {
 
         if ($(this).val() == "=") {
             operator = undefined;
-        }else {
+        } else {
             operator = $(this).val();
         }
         currentNum = 0;
     });
-});
+})
+;
